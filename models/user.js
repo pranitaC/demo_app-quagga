75dbd207221f6ab7aee3539dbdc7e5a969e3cf7e
Pranita Chuadhari <pranitac9793@gmail.com>
2016-08-31 16:29:06 +0530

    Contains sign up,login,logout routes

--- models/user.js
+++ models/user.js
@@ -3,11 +3,16 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-  name: String,
-  username: String,
-  password: String
+  name: { type: String, required: true, min: 3, max: 64 },
+  username: { type: String, required: true, min: 3, max: 64 },
+  password: { type: String, required: true, min: 4, max: 8 }
 });
 
+/**The plugin simplifies building username and password login with passport 
+    it also add some methods to Schema
+    e.g. register
+         authenticate
+         serialize                   **/
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
