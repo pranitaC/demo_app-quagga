44049fd775f29c97beb3a98a55f6ee8dc011a353
Pranita Chuadhari <pranitac9793@gmail.com>
2016-08-31 16:41:12 +0530

    Contains vehicle create show route

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,19 +1,27 @@
 var express = require('express');
-var passport = require('passport');
 var router = express.Router();
 var Vehicle = require('models/vehicle');
 var Schema = require('mongoose').Schema;
+var authenticate = require('authenticate');
 
-// Requesting vehicle details of the logged in user
-router.get('/', passport.authenticate('jwt',{ session: false }), function(req, res, next) {
+/** Requesting vehicle details of the logged in user  
+*   This authenticate the user with JWT and 
+*   Find the vehicles of the user using user_id
+*   And send the vehicles to the client
+**/
+router.get('/', authenticate , function(req, res, next) {
   Vehicle.find({ user_id: req.user._id },function(err, vehicles){
     res.send(vehicles);
   })
 });
 
-//Creating new vehicle details of the logged in user  
-router.post('/', passport.authenticate('jwt',{ session: false }), function(req, res){
+/**Creating new vehicle details of the logged in user 
+*  This authenticate the user with JWT and 
+*   And save the vehicle of the user using user_id
+**/
+router.post('/', authenticate , function(req, res){
   var vehicle = new Vehicle(req.body.vehicle);
+  vehicle.user_id = req.user._id;
   
   vehicle.save(function(err, vehicle){
     if(err){
