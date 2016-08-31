cdc939d3e1a8f41c7dd3397670a1114f0bd17091
Pranita Chuadhari <pranitac9793@gmail.com>
2016-08-31 16:30:10 +0530

    Contains vehicle Create and Show route

--- models/vehicle.js
+++ models/vehicle.js
@@ -5,8 +5,7 @@ var Vehicle = new Schema({
   make: { type: String, required: true, min: 2, max: 64 },
   regno: { type: String, required: true, min: 10, max: 10 },
   user_id: { type: Schema.ObjectId, required: true },
-  model: { type: String, required: false },
-  trim: { type: String, required: false }
+  model: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Vehicle', Vehicle);
