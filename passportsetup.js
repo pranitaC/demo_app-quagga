f824169435e2e033773f8da43ebd819578d1e8d6
Pranita Chuadhari <pranitac9793@gmail.com>
2016-08-31 16:31:59 +0530

    A seperate module for passport-jwt strategy

--- passportsetup.js
+++ passportsetup.js
@@ -8,7 +8,13 @@ var secret = require('config/secret');
 var User = require('models/user');
 var Schema = require('mongoose').Schema;
 
-// User Authentication using Passport strategy with JWT
+/* User Authentication using Passport strategy with JWT 
+   fromAuthHeader creates a new extractor that looks for JWT in authorization header
+   jwtFromRequest accepts a request and returns either the JWT as a string or null
+   secretOrKey is a required string containing a key to verify a token 
+   Using Jwt and secret JwtStrategy decrypts the user id and store it in jwt_payload using which user is verified
+   and send response as a user else error
+*/
 module.exports = function(app) {
   app.use(passport.initialize());
   passport.use(User.createStrategy());
